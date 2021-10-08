e7d9a6e102d134d5ece85d9ec57d3d8eb93eb855
wh-debug <1813763848@qq.com>
2021-10-08 21:40:55 +0800

    VGG16_Test

--- Min_Data_CNN.py
+++ Min_Data_CNN.py
@@ -78,19 +78,19 @@ for fname in fnames:
     dst = os.path.join(test_dogs_dir, fname)
     shutil.copyfile(src, dst)
 
-train_datagen = ImageDataGenerator(rescale=1./255)
+train_datagen = ImageDataGenerator(rescale=1./255, rotation_range=40, width_shift_range=0.2, height_shift_range=0.2,shear_range=0.2, zoom_range=0.2,horizontal_flip=True)
 test_datagen = ImageDataGenerator(rescale=1./255)
 
 train_generator = train_datagen.flow_from_directory(
     train_dir,
     target_size=(150, 150),
-    batch_size=20,
+    batch_size=32,
     class_mode='binary'
     )
 validation_generator = test_datagen.flow_from_directory(
     validation_dir,
     target_size=(150, 150),
-    batch_size=20,
+    batch_size=32,
     class_mode='binary'
     )
 
@@ -104,6 +104,7 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Flatten())
+model.add(layers.Dropout(0.5))
 model.add(layers.Dense(512, activation='relu'))
 model.add(layers.Dense(1, activation='sigmoid'))
 # print(model.summary())
@@ -112,11 +113,11 @@ model.compile(loss='binary_crossentropy', optimizer=optimizers.RMSprop(lr=1e-4),
 history = model.fit_generator(
     train_generator,
     steps_per_epoch=100,
-    epochs=30,
+    epochs=100,
     validation_data=validation_generator,
     validation_steps=50
 )
-model.save('cat_and_dogs_small_1.h5')
+model.save('cat_and_dogs_small_2.h5')
 
 acc = history.history['acc']
 val_acc = history.history['val_acc']
